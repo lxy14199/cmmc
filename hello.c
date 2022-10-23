5dec4f33b93f5bb86b6d707eab7dd02730426d2b
lxy <2380632137@qq.com>
2022-10-23 12:45:04 +0000

    fix bug

--- hello.c
+++ hello.c
@@ -2,21 +2,21 @@
 
 int fibonacci(int i) {
 		if (i <= 1) {
-				        return 1;
-						    
+				return 1;
+
 		}
-		    return fibonacci(i-1) + fibonacci(i-2);
-			
+		return fibonacci(i-1) + fibonacci(i-2);
+
 }
 
 int main()
 {
-		    int i;
-			    i = 0;
-				while (i <= 10) {
-						printf("fibonacci(%2d) = %d\n", i, fibonacci(i));
-						i = i + 1;						    
-				}
-				return 0;
-					
+		int i;
+		i = 0;
+		while (i <= 10) {
+				printf("fibonacci(%2d) = %d\n", i, fibonacci(i));
+				i = i + 1;						    
+		}
+		return 0;
+
 }
